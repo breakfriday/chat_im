fe31ab442994ffcdc9ca6970afa4fda83e7d59eb
螺旋 <dupenghui@cai-inc.com>
2022-12-08 15:35:46 +0800

    feat:update

--- src/layouts/ProLayout/index.tsx
+++ src/layouts/ProLayout/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import { Outlet, Link, useLocation } from 'ice';
+import ProLayout from '@ant-design/pro-layout';
+import { asideMenuConfig } from '@/menuConfig';
+import AvatarDropdown from '@/components/AvatarDropdown';
+import store from '@/store';
+import logo from '@/assets/logo.png';
+import styles from './layout.module.css';
+import Footer from '@/components/Footer';
 
-function ProLayout() {
+export default function Layout() {
+  const location = useLocation();
+  const [userState] = store.useModel('user');
+
+  if (['/login'].includes(location.pathname)) {
+    return <Outlet />;
+  }
+
   return (
-    <div>
-      ProLayout
-    </div>
+    <ProLayout
+      menu={{ defaultOpenAll: true }}
+      className={styles.layout}
+      logo={<img src={logo} alt="logo" />}
+      title="ICE Pro"
+      location={{
+        pathname: location.pathname,
+      }}
+      layout="mix"
+      rightContentRender={() => (
+        <AvatarDropdown avatar={userState.currentUser.avatar} name={userState.currentUser.name} />
+      )}
+      menuDataRender={() => asideMenuConfig}
+      menuItemRender={(item, defaultDom) => {
+        if (!item.path) {
+          return defaultDom;
+        }
+        return <Link to={item.path}>{defaultDom}</Link>;
+      }}
+      footerRender={() => <Footer />}
+    >
+      <Outlet />
+    </ProLayout>
   );
 }
-
-export default ProLayout;
