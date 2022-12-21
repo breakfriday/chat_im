40a1d6d74fdafcef2bf1950efe083e6370fe41a6
螺旋 <dupenghui@cai-inc.com>
2022-12-21 17:49:22 +0800

    feat:update

--- packages/chatui/components/IconButton/index.js
+++ packages/chatui/components/IconButton/index.js
@@ -1,22 +1,24 @@
-import _extends from "@babel/runtime/helpers/esm/extends";
-import _objectWithoutProperties from "@babel/runtime/helpers/esm/objectWithoutProperties";
-var _excluded = ["className", "icon", "img"];
+import _extends from '@babel/runtime/helpers/esm/extends';
+import _objectWithoutProperties from '@babel/runtime/helpers/esm/objectWithoutProperties';
 import React from 'react';
 import clsx from 'clsx';
 import { Button } from '../Button';
 import { Icon } from '../Icon';
-export var IconButton = function IconButton(props) {
-  var className = props.className,
-      icon = props.icon,
-      img = props.img,
-      other = _objectWithoutProperties(props, _excluded);
 
-  return /*#__PURE__*/React.createElement(Button, _extends({
-    className: clsx('IconBtn', className)
-  }, other), icon && /*#__PURE__*/React.createElement(Icon, {
-    type: icon
-  }), !icon && img && /*#__PURE__*/React.createElement("img", {
+const _excluded = ['className', 'icon', 'img'];
+
+export var IconButton = function IconButton(props) {
+  const { className } = props;
+  const { icon } = props;
+  const { img } = props;
+  const other = _objectWithoutProperties(props, _excluded);
+
+  return /* #__PURE__ */React.createElement(Button, _extends({
+    className: clsx('IconBtn', className),
+  }, other), icon && /* #__PURE__ */React.createElement(Icon, {
+    type: icon,
+  }), !icon && img && /* #__PURE__ */React.createElement('img', {
     src: img,
-    alt: ""
+    alt: '',
   }));
 };
