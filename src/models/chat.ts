bd38dc309204915b55f6a9f667f946ebd77d5e5e
螺旋 <dupenghui@cai-inc.com>
2022-12-21 15:31:27 +0800

    feat:update

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -2,16 +2,20 @@ import chat_service from '@/services/Chat/index';
 
 export default {
   state: {
-
+    session_id: '',
+    accid: '',
+    target_accid: '',
+    message: '',
   },
   reducers: {
+    update(prevState, payload) {
+      return { ...prevState, ...payload };
+    },
 
   },
   effects: (dispatch) => ({
     async connet_chat() {
       const chat = new chat_service();
-
-      
     },
 
   }),
